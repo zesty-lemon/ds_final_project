a53dc9f5244788ee73bb3f8ef421dcf1419c349c
Giles Lemmon <51659929+zesty-lemon@users.noreply.github.com>
2025-10-06 11:51:02 -0400

    add datetime column

--- initial_poc.py
+++ initial_poc.py
@@ -10,9 +10,13 @@ music_dir = "/apple_music/20220119"
 df = pd.DataFrame()
 input_file_apple_music = os.path.join(base_path + music_dir, "new_york_city.xlsx")
 
-df = pd.DataFrame()
-
 df = pd.read_excel(input_file_apple_music)
+
+# file contains weird dates "07-21-2021 : 1".  Strip out the IDS ": 1" and convert to datetime
+df['date_dt'] = pd.to_datetime(
+    df['DATE'].astype(str).str.split(':').str[0].str.strip(), # get rid of everything after a :
+    errors='coerce'
+)
 
 print("done")
 
